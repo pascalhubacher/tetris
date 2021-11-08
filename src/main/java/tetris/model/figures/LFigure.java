d78073400c421560f4309ee2cb59037a3654e33c
Pascal Hubacher <pascal.hubacher@outlook.com>
2021-11-08 01:15:58 +0100

    override rotate method

--- src/main/java/tetris/model/figures/LFigure.java
+++ src/main/java/tetris/model/figures/LFigure.java
@@ -3,7 +3,10 @@ package tetris.model.figures;
 import tetris.gui.Block;
 import tetris.model.Figure;
 
+import java.util.ArrayList;
+
 public class LFigure extends Figure {
+
     public LFigure(int x, int y) {
         super(x, y);
         blocks[0] = new Block(x - 1, y, COLOR);
@@ -12,4 +15,19 @@ public class LFigure extends Figure {
         blocks[3] = new Block(x + 1, y + 1, COLOR);
     }
 
+    @Override
+    public void rotate(int d) {
+        switch (d) {
+            case 1 -> {
+                for (Block block : blocks) {
+                    int tempBlock = blocks[1].y - (block.x - blocks[1].x);
+                    block.x = (block.y - blocks[1].y) + blocks[1].x;
+                    block.y = tempBlock;
+                }
+            }
+            case -1 -> {
+                // missing
+            }
+        }
+    }
 }
