c4cb6992d737137a35fb171a852a3eb1c2e062db
Pascal Hubacher <pascal.hubacher@outlook.com>
2021-11-08 20:02:09 +0100

    changed point to rotate for J and L figure

--- src/main/java/tetris/model/figures/JFigure.java
+++ src/main/java/tetris/model/figures/JFigure.java
@@ -4,24 +4,41 @@ import tetris.gui.Block;
 import tetris.model.Figure;
 
 public class JFigure extends Figure {
+
+    private int[] rotateBlock = new int[2];
+
     public JFigure(int x, int y) {
         super(x, y);
         blocks[0] = new Block(x - 1, y, COLOR);
         blocks[1] = new Block(x, y, COLOR);
         blocks[2] = new Block(x + 1, y, COLOR);
         blocks[3] = new Block(x + 1, y - 1, COLOR);
+        rotateBlock[0] = blocks[1].x;
+        rotateBlock[1] = blocks[1].y - 1;
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        //this.move(dx, dy);
+        for (Block block : blocks) {
+            block.x += dx;
+            block.y += dy;
+        }
+        rotateBlock[0] += dx;
+        rotateBlock[1] += dy;
     }
 
     @Override
     public void rotate(int d) {
-        System.out.println("test");
         switch (d) {
             case 1 -> {
                 for (Block block : blocks) {
-                    int tempBlock = blocks[1].y + 1 - (block.x - blocks[1].x);
-                    block.x = (block.y - blocks[1].y + 1) + blocks[1].x;
+                    int tempBlock = rotateBlock[1] - (block.x - rotateBlock[0]);
+                    block.x = (block.y - rotateBlock[1]) + rotateBlock[0];
                     block.y = tempBlock;
                 }
+                // rotate rotateBlock
+                //tempBlock = rotateBlock[1] - (block.x - rotateBlock[0]);
             }
             case -1 -> {
                 // missing
--- src/main/java/tetris/model/figures/LFigure.java
+++ src/main/java/tetris/model/figures/LFigure.java
@@ -5,12 +5,27 @@ import tetris.model.Figure;
 
 public class LFigure extends Figure {
 
+    private int[] rotateBlock = new int[2];
+
     public LFigure(int x, int y) {
         super(x, y);
         blocks[0] = new Block(x - 1, y, COLOR);
         blocks[1] = new Block(x, y, COLOR);
         blocks[2] = new Block(x + 1, y, COLOR);
         blocks[3] = new Block(x + 1, y + 1, COLOR);
+        rotateBlock[0] = blocks[1].x;
+        rotateBlock[1] = blocks[1].y + 1;
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        //this.move(dx, dy);
+        for (Block block : blocks) {
+            block.x += dx;
+            block.y += dy;
+        }
+        rotateBlock[0] += dx;
+        rotateBlock[1] += dy;
     }
 
     @Override
@@ -18,10 +33,12 @@ public class LFigure extends Figure {
         switch (d) {
             case 1 -> {
                 for (Block block : blocks) {
-                    int tempBlock = blocks[1].y - (block.x - blocks[1].x);
-                    block.x = (block.y - blocks[1].y) + blocks[1].x;
+                    int tempBlock = rotateBlock[1] - (block.x - rotateBlock[0]);
+                    block.x = (block.y - rotateBlock[1]) + rotateBlock[0];
                     block.y = tempBlock;
                 }
+                // rotate rotateBlock
+                //tempBlock = rotateBlock[1] - (block.x - rotateBlock[0]);
             }
             case -1 -> {
                 // missing
